07e4f52344a953a145feccf5861e6a6058c2e13a
Kamila Adamska <kamilaaadamska@gmail.com>
2023-06-02 12:14:52 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NgbDatepickerModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+import { CalendarComponent } from './home/calendar/calendar.component';
+import { NewsComponent } from './home/news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent,
+    CalendarComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NgbDatepickerModule,
+    FormsModule,
+    JsonPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
